3386cef20d2f8093808478d2071ff72dc8025d8f
dayaGuerra <dayamandalaman@gmail.com>
2019-03-18 03:40:13 -0500

    Revert " Comprobando push a master"
    This reverts commit 8a25f92ca722a37f4478a7d62c463bec3a5082af.

---
+++ LIB/modules/validate.js
@@ -0,0 +1,34 @@
+import linksDeRutas from './joinLink';
+
+const fetch = require('node-fetch');
+
+const validarUrl = (arraydeobjetos) => {
+  const arrlinks = linksDeRutas(arraydeobjetos);
+  const evaluarLinks = arrlinks.map(propiedad => new Promise((resolve) => {
+    fetch(propiedad.link)
+      .then((res) => {
+        if (res.status > 199 && res.status < 400) {
+          resolve({
+            ...propiedad,
+            status: res.status,
+            statusText: res.statusText,
+          });
+        } else {
+          resolve({
+            ...propiedad,
+            status: res.status,
+            statusText: 'fail',
+          });
+        }
+      })
+      .catch(() => {
+        resolve({
+          ...propiedad,
+          status: 'no existe',
+          statusText: 'fail',
+        });
+      });
+  }));
+  return Promise.all(evaluarLinks);
+};
+export default validarUrl;
