d7227912facc5b3bb84a02fc1ef8a0e4fac8c7a3
dayaGuerra <dayamandalaman@gmail.com>
2019-03-21 18:07:35 -0500

    hacer de la funcion cli una promesa

--- src/modules/cli.js
+++ src/modules/cli.js
@@ -14,42 +14,44 @@ var _validate = _interopRequireDefault(require("./validate"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var cli = function cli(arg) {
-  if (arg.length === 0) {
-    console.log('Ingresa una ruta valida');
-  }
-
-  if (arg.length === 3) {
-    // respuesta stats + validate
-    if (arg[1] === '--stats' && arg[2] === '--validate' || arg[2] === '--validate' && arg[1] === '--stats') {
-      var links = (0, _joinLink.default)(arg[0]);
-      (0, _validate.default)(arg[0]).then(function (response) {
-        console.log(" Total:  ".concat((0, _stats.funcionStats)(links).total, "\n unique: ").concat((0, _stats.funcionStats)(links).unicos, "\n Broken: ").concat((0, _stats.statsRotos)(response)));
-      });
-    }
-  } // respuesta stats o validate
-
-
-  if (arg.length === 2) {
-    if (arg[1] === '--stats') {
-      var _links = (0, _joinLink.default)(arg[0]);
-
-      console.log(" Total:  ".concat((0, _stats.funcionStats)(_links).total, "\n unique: ").concat((0, _stats.funcionStats)(_links).unicos));
+  return new Promise(function (resolve) {
+    if (arg.length === 0) {
+      resolve('Ingresa una ruta valida');
     }
 
-    if (arg[1] === '--validate') {
-      (0, _validate.default)(arg[0]).then(function (response) {
-        return response.forEach(function (ele) {
-          return console.log("".concat(ele.ruta, " ").concat(ele.link, " ").concat(ele.texto, " ").concat(ele.status, " ").concat(ele.statusText));
+    if (arg.length === 3) {
+      // respuesta stats + validate
+      if (arg[1] === '--stats' && arg[2] === '--validate' || arg[2] === '--validate' && arg[1] === '--stats') {
+        var links = (0, _joinLink.default)(arg[0]);
+        (0, _validate.default)(arg[0]).then(function (response) {
+          resolve(" Total:  ".concat((0, _stats.funcionStats)(links).total, "\n unique: ").concat((0, _stats.funcionStats)(links).unicos, "\n Broken: ").concat((0, _stats.statsRotos)(response)));
         });
+      }
+    } // respuesta stats o validate
+
+
+    if (arg.length === 2) {
+      if (arg[1] === '--stats') {
+        var _links = (0, _joinLink.default)(arg[0]);
+
+        resolve(" Total:  ".concat((0, _stats.funcionStats)(_links).total, "\n unique: ").concat((0, _stats.funcionStats)(_links).unicos));
+      }
+
+      if (arg[1] === '--validate') {
+        (0, _validate.default)(arg[0]).then(function (response) {
+          return response.forEach(function (ele) {
+            return resolve("".concat(ele.ruta, " ").concat(ele.link, " ").concat(ele.texto, " ").concat(ele.status, " ").concat(ele.statusText));
+          });
+        });
+      }
+    }
+
+    if (arg.length === 1) {
+      (0, _joinLink.default)(arg[0]).forEach(function (ele) {
+        return resolve("".concat(ele.ruta, " ").concat(ele.link, " ").concat(ele.texto));
       });
     }
-  }
-
-  if (arg.length === 1) {
-    (0, _joinLink.default)(arg[0]).forEach(function (ele) {
-      return console.log("".concat(ele.ruta, " ").concat(ele.link, " ").concat(ele.texto));
-    });
-  }
+  });
 };
 
 var _default = cli;
