6442c9a1c2eb344e503a936e1d841d316baa7da0
dayaGuerra <dayamandalaman@gmail.com>
2019-03-22 02:54:05 -0500

    corrigiendo descripción del test

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -13,10 +13,12 @@ describe('cli', () => {
     expect(typeof cli).toBe('function');
   });
   it('deberia de retornar error', () => cli(arg).then(response => expect(response).toEqual('Ingresa una ruta valida')));
-  it('deberia de retornar un string con dos datos', () => cli(arg1).then(response => expect(response).toEqual('Total:4\nunique:1')));
-  it('deberia de retornar un string con dos datos', () => cli(arg2).then(response => expect(response).toEqual(
-    `${path.resolve(`${process.cwd()}/test/prueba/prueba1/documento4.md`)} https://nodejs.org/en/ hola 200 OK`)));
-  it('deberia de retornar un string con dos datos', () => cli(arg3).then(response => expect(response).toEqual('Total:4\nunique:1\nBroken:3')));
-  it('deberia de retornar un string con dos datos', () => cli(arg4).then(response => expect(response).toEqual(
-    `${path.resolve(`${process.cwd()}/test/prueba/prueba1/documento4.md`)} https://nodejs.org/en/ hola`)));
+  it('deberia de retornar un string con dos datos estadisticos', () => cli(arg1).then(response => expect(response).toEqual('Total:4\nunique:1')));
+  it('deberia de retornar un string con los datos validados', () => cli(arg2).then(response => expect(response).toEqual(
+    `${path.resolve(`${process.cwd()}/test/prueba/prueba1/documento4.md`)} https://nodejs.org/en/ hola 200 OK`,
+  )));
+  it('deberia de retornar un string con tres datos estadisticos', () => cli(arg3).then(response => expect(response).toEqual('Total:4\nunique:1\nBroken:3')));
+  it('deberia de retornar un string con los datos sin validar', () => cli(arg4).then(response => expect(response).toEqual(
+    `${path.resolve(`${process.cwd()}/test/prueba/prueba1/documento4.md`)} https://nodejs.org/en/ hola`,
+  )));
 });
